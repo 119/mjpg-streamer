086983dfb61e86dadfaa9c5b6242d134891e60b3
k@raspberrypi <k@raspberrypi.com>
2015-01-21 19:03:10 +0800

    03-mjpg-streamer-树莓派编译-进展-链接失败.txt

---
+++ mjpg-streamer/3-build.sh
@@ -0,0 +1,1 @@
+make CC=arm-linux-gnueabihf-gcc CFLAGS="-I/home/k/1-git/4-mjpg-streamer/mjpg-streamer/2-lib/jpeg-6b -I./ -I/home/k/1-git/4-mjpg-streamer/mjpg-streamer/2-lib/build-RPi -L/home/k/1-git/4-mjpg-streamer/mjpg-streamer/2-lib/build-RPi"
